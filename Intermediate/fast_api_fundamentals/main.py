1c0592990afb3780f41a03188bc1c8134f046c58
Marlon Menendez <marlon.menendezg@gmail.com>
2023-04-21 07:04:43 -0600

    Update the main file

--- Intermediate/fast_api_fundamentals/main.py
+++ Intermediate/fast_api_fundamentals/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+# Models
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+# Request and Response Body
+@app.post("/user/new")
+def create_user(person: Person = Body(...)):
+    return person
