01f0a42e1b696ae97f1cf1879bf0f8d9ef60a236
Marlon Menendez <marlon.menendezg@gmail.com>
2023-04-24 20:10:58 -0600

    Update of the main app

--- Intermediate/fast_api_fundamentals/main.py
+++ Intermediate/fast_api_fundamentals/main.py
@@ -1,11 +1,11 @@
 # Python
-from typing import Optional
+from typing import Optional, Dict
 
 # Pydantic
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -31,12 +31,30 @@ def create_user(person: Person = Body(...)) -> Person:
     # The three dots in fastAPI means that it is obligatory
     return person
 
-# Validate Query Parameters
-@app.get("/person/detail")
+
+# Validate Path Parameters
+@app.get("person/detail/{person_id}")
 def get_person_details(
-    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
-    age: Optional[int] = Query(default=None)
-):
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person ID",
+        description="The identifier number of the person. This is obligatory."
+    ),
+    name: Optional[str] = Query(
+        default=None,
+        min_length=1,
+        max_length=50,
+        title="Person Name",
+        description="This is the name of the person. It's between 1 and 50 chars lenght",
+    ),
+    age: Optional[int] = Query(
+        default=None,
+        title="Person Age",
+        description="This is the age of the person."
+    )
+) -> Dict:
     return {
+        person_id: "This person exists!",
         name: age
     }
